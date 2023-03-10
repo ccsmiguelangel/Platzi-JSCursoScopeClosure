f2145198ad9081033b2be6e229c378fb70bb4b4c
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2023-03-09 21:19:08 -0400

    :art: Added code class lesson 1
    Lesson 1 Scope and danger global scope to dont declare varible

---
+++ src/scope/global.js
@@ -0,0 +1,29 @@
+var a; // declarar
+var b = 'b'; //declaramos / asignamos
+
+b = 'bb'; // reasignar
+
+var a = 'aa'; //redeclaracion
+// --
+
+// Global Scope
+var fruit = 'apple'; // Global
+console.log(fruit);
+
+function bestFruit(){ //Bloque
+  console.log(fruit);
+}
+
+bestFruit(); // visualizamos la variable fruit
+// --
+
+// CUIDADO
+// Al no declarar una variable pasa al scope global
+
+function countries(){
+  country = 'Colombia'; // global
+  console.log(country);
+}
+
+countries();
+console.log(country);
