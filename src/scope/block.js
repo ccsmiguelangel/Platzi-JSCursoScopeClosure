717d599156615c7cafa61a9fe219aa8719c8eaa7
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2023-03-09 21:41:57 -0400

    :white_check_mark: Added file updates in block.js
    Block js expample of var function scope

--- src/scope/block.js
+++ src/scope/block.js
@@ -3,7 +3,7 @@
 function fruits(){
   // {} Todo lo que está dentro de handlevar son bloques
   if(true){
-    var fruit1 = 'Apple'; // funtion scope
+    var fruit1 = 'Apple'; // funtion scope | Este es el problema la idea es no romper la lógica, por eso usualmente no usamos var
     let fruit2 = 'Kiwi'; // block scope
     const fruit3 = 'Banana'; // block scope
   }
