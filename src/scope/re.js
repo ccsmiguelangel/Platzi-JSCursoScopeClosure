dede6f1a92fdaa98333fd23c63f6abaf7c06cef1
ccsmiguelangel <miguelangel1074.gc@gmail.com>
2023-03-13 22:35:11 -0400

    :art: reasign declare variables lesson
    Used const, var and let

---
+++ src/scope/re.js
@@ -0,0 +1,29 @@
+var firstName; // declarando (se le asigna auto undefined)
+firstName = 'Miguel';
+// console.log(firstName);
+
+var lastName = 'Pena'; //Asignando y declarando
+lastName = 'Sanchez'; // reasignamos
+// console.log(lastName); // Sanchez
+
+var secondName = 'David'; // declarando asignando
+var secondName = 'Juan'; // redeclarando reasignando
+// console.log(secondName); // Juan
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+// let fruit = 'Banana'; //error
+// console.log(fruit); //Kiwi
+
+const animal = 'dog'; //declarado asignado
+// animal = 'cat'; // redeclarado reasignado
+// const animal = 'snake'; //error
+// console.log(animal);
+
+const vehicles = [];
+vehicles.push('🚗');
+// console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles)
+// Estamos haciendo referencia, no estamos declarando
